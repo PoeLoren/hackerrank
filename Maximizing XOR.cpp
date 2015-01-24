d1541fd0610947786fbec97ace008b3838cc4f9b
Poe <yxluozengbo@gmail.com>
2015-01-24 15:46:37 +0800

    Maximizing XOR

---
+++ Maximizing XOR.cpp
@@ -0,0 +1,45 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <cstdlib>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+ * Complete the function below.
+ */
+int maxXor(int l, int r) {
+    int ret = 0;
+    for(int i = l; i <= r; i++){
+        for(int j = i; j <= r; ++j){
+            int res = i ^ j;
+            ret = max(ret, res);
+        }
+    }
+    return ret;
+}
+
+int main() {
+    int res;
+    int _l;
+    cin >> _l;
+    
+    int _r;
+    cin >> _r;
+    
+    res = maxXor(_l, _r);
+    cout << res;
+    
+    return 0;
+}
