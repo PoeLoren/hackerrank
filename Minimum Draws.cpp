a424f291331c8b3c17804405d5963d53efc6a0ef
Poe <yxluozengbo@gmail.com>
2015-01-27 21:47:26 +0800

    Minimum Draws

---
+++ Minimum Draws.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin >> n;
+    while(n--){
+        int k;
+        cin >> k;
+        cout << k+ 1 << endl;
+    }
+    return 0;
+}
